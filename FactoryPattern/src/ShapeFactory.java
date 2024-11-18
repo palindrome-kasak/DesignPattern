3bf7bc930b588c9c865290a400ee0c9fc73aaf16
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-11-18 12:51:24 +0530

    Factory Pattern

---
+++ FactoryPattern/src/ShapeFactory.java
@@ -0,0 +1,14 @@
+public class ShapeFactory {
+    Shape getShape(String s){
+        switch (s){
+            case "CIRCLE":
+                return new Circle();
+            case "RECTANGLE":
+                return new Rectangle();
+            case "SQUARE":
+                return new Square();
+            default:
+                return null;
+        }
+    }
+}
